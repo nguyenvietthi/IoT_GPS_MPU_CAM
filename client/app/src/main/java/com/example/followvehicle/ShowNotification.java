89379ede6f4f3edb3387570c1cfe46370d35b3b9
nguyenvietthi <nguyenvietthi.nd@gmail.com>
2024-06-22 22:59:26 +0700

    update notification

--- client/app/src/main/java/com/example/followvehicle/ShowNotification.java
+++ client/app/src/main/java/com/example/followvehicle/ShowNotification.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -26,19 +28,36 @@ public class ShowNotification extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_show_notification, container, false);
-        recyclerView = view.findViewById(R.id.recyclerViewNotifications);
 
-        // Dữ liệu mẫu cho thông báo
-//        notificationList = new ArrayList<>();
-//        notificationList.add("Thông báo 1");
-//        notificationList.add("Thông báo 2");
-//        notificationList.add("Thông báo 3");
+        ListView listView =view.findViewById(R.id.notification_list);
+
         notificationList = StoreUserData.getInstance(requireContext()).getNotifications();
         Collections.reverse(notificationList);
-        // Khởi tạo adapter và gán cho RecyclerView
-        adapter = new NotificationAdapter(notificationList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(adapter);
+        List<Item> items = new ArrayList<>();
+
+        for(String str:notificationList){
+            items.add(new Item(StoreUserData.getInstance(requireContext()).getKeyDeviceCode(), str));
+        }
+
+
+
+
+        CustomAdapter adapter = new CustomAdapter(requireContext(), R.layout.list_item, items);
+        listView.setAdapter(adapter);
+
+//        recyclerView = view.findViewById(R.id.recyclerViewNotifications);
+//
+//        // Dữ liệu mẫu cho thông báo
+////        notificationList = new ArrayList<>();
+////        notificationList.add("Thông báo 1");
+////        notificationList.add("Thông báo 2");
+////        notificationList.add("Thông báo 3");
+//        notificationList = StoreUserData.getInstance(requireContext()).getNotifications();
+//        Collections.reverse(notificationList);
+//        // Khởi tạo adapter và gán cho RecyclerView
+//        adapter = new NotificationAdapter(notificationList);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        recyclerView.setAdapter(adapter);
 
         return view;
     }
